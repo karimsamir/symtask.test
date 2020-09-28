1ee23fd527151dd793be2c14929bd69187cb2f2b
Karim Samir <karim5977@gmail.com>
2020-09-28 10:23:12 +0200

    redirect to homepage in auth

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -97,7 +97,9 @@ class CustomAuthenticator extends AbstractFormLoginAuthenticator implements Pass
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // redirect to some "app_homepage" route - of wherever you want
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
