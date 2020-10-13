356979b4990f32b8d50c21118a88634b76125c6a
Karim Samir <karim5977@gmail.com>
2020-10-13 18:00:35 +0200

    add registration form

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -99,7 +99,7 @@ class CustomAuthenticator extends AbstractFormLoginAuthenticator implements Pass
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
         // redirect to some "app_homepage" route - of wherever you want
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('homepage'));
     }
 
     protected function getLoginUrl()
