1c4a4526803a9e28a4a3fcc60e97a0476f1f5c9d
Karim Samir <karim5977@gmail.com>
2020-10-25 17:44:41 +0200

    change to quiz homepage when login

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -99,7 +99,7 @@ class CustomAuthenticator extends AbstractFormLoginAuthenticator implements Pass
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
         // redirect to some "app_homepage" route - of wherever you want
-        return new RedirectResponse($this->urlGenerator->generate('homepage'));
+        return new RedirectResponse($this->urlGenerator->generate('quiz_homepage'));
     }
 
     protected function getLoginUrl()
